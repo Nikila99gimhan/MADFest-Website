383d44cfc0ed16120a2d368cba48df95dad24fb6
Srilal Sachintha <SrilalSachintha@Gmail.com>
2022-03-31 19:23:44 +0530

    init

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app dark>
+    <v-app-bar class="NavBarAcrylic" color="transparent" fixed app>
+      <v-toolbar-title>
+        <v-img width="256" contain :src="require('~/assets/logo_alt.png')"/>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn color="secondary" large>Register NOW!</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer absolute app>
+      <span>&copy; {{ new Date().getFullYear() }} IEEE Computer Society Student Chapter of NSBM</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'MADFest'
+    }
+  }
+}
+</script>
+
+<style>
+.NavBarAcrylic{
+  background-color: transparent;
+  backdrop-filter: blur(10px);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row no-gutters justify="center">
+      <v-img contain max-width="50%" :src="require('~/assets/logo.png')"></v-img>
+    </v-row>
+
+    <v-row no-gutters justify="center">
+      <v-img contain max-width="50%" :src="require('~/assets/logo_line.png')"></v-img>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+
+</style>
