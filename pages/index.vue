d593140c514701c9467a9627add71580d0b5f182
Srilal Sachintha <SrilalSachintha@Gmail.com>
2022-03-31 19:42:54 +0530

    logo change

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <v-img contain max-width="50%" :src="require('~/assets/logo.png')"></v-img>
     </v-row>
 
-    <v-row no-gutters justify="center">
+    <v-row class="mt-8" no-gutters justify="center">
       <v-img contain max-width="50%" :src="require('~/assets/logo_line.png')"></v-img>
     </v-row>
   </v-container>
