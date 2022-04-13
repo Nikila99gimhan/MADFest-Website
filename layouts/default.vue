3ca3ec82a46c5d8f576b663e679cc674d79a0c89
prineth <prinethfernandox@gmail.com>
2022-04-13 14:16:15 +0530

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,18 @@
 <template>
   <v-app dark>
-
-    <div class="area" >
-      <ul class="circles">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-    </div >
-
     <v-app-bar class="NavBarAcrylic" color="transparent" fixed app>
       <v-toolbar-title>
         <v-img width="256" contain :src="require('~/assets/logo_alt.png')"/>
       </v-toolbar-title>
       <v-spacer />
-      <v-btn large target="_blank" href="https://ieeensbm.org" icon>
-        <v-icon>mdi-web</v-icon>
-      </v-btn>
-<!--      <v-btn color="secondary" large>Register NOW!</v-btn>-->
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-app-bar>
     <v-main class="NavBarAcrylic">
       <v-container>
@@ -43,23 +31,38 @@
 export default {
   data () {
     return {
-      title: 'MADFest'
-    }
-  }
-}
+      title: "MADFest",
+      loader: null,
+      loading4: false,
+      items: [
+        { title: "Home", route: "/" },
+        { title: "Content", route: "/content" },
+        { title: "Settings", route: "/settings" },
+      ],
+    };
+  },
+
+  watch: {
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
+
+      setTimeout(() => (this[l] = false), 3000);
+
+      this.loader = null;
+    },
+  },
+};
 </script>
 
 <style>
-.NavBarAcrylic{
+* {
+  font-family: "Poppins";
+}
+.NavBarAcrylic {
   background-color: transparent;
   backdrop-filter: blur(10px);
 }
-
-
-body{
-  font-family: 'Exo', sans-serif;
-}
-
 
 .context {
   width: 100%;
@@ -80,106 +83,6 @@ body{
   width: 100%;
   height:100vh;
 }
-
-.circles{
-  position: absolute;
-  color: #32ad4a;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-}
-
-.circles li{
-  position: absolute;
-  display: block;
-  list-style: none;
-  width: 20px;
-  height: 20px;
-  background: rgb(18, 95, 169);
-  animation: animate 25s linear infinite;
-  bottom: -150px;
-
-}
-
-.circles li:nth-child(1){
-  left: 25%;
-  width: 80px;
-  height: 80px;
-  animation-delay: 0s;
-}
-
-
-.circles li:nth-child(2){
-  left: 10%;
-  width: 20px;
-  height: 20px;
-  animation-delay: 2s;
-  animation-duration: 12s;
-}
-
-.circles li:nth-child(3){
-  left: 70%;
-  width: 20px;
-  height: 20px;
-  animation-delay: 4s;
-}
-
-.circles li:nth-child(4){
-  left: 40%;
-  width: 60px;
-  height: 60px;
-  animation-delay: 0s;
-  animation-duration: 18s;
-}
-
-.circles li:nth-child(5){
-  left: 65%;
-  width: 20px;
-  height: 20px;
-  animation-delay: 0s;
-}
-
-.circles li:nth-child(6){
-  left: 75%;
-  width: 110px;
-  height: 110px;
-  animation-delay: 3s;
-}
-
-.circles li:nth-child(7){
-  left: 35%;
-  width: 150px;
-  height: 150px;
-  animation-delay: 7s;
-}
-
-.circles li:nth-child(8){
-  left: 50%;
-  width: 25px;
-  height: 25px;
-  animation-delay: 15s;
-  animation-duration: 45s;
-}
-
-.circles li:nth-child(9){
-  left: 20%;
-  width: 15px;
-  height: 15px;
-  animation-delay: 2s;
-  animation-duration: 35s;
-}
-
-.circles li:nth-child(10){
-  left: 85%;
-  width: 150px;
-  height: 150px;
-  animation-delay: 0s;
-  animation-duration: 11s;
-}
-
-
 
 @keyframes animate {
 
