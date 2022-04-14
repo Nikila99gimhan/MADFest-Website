d39960dab9ae208ecd5dc4200983574b29a60c7d
prineth <prinethfernandox@gmail.com>
2022-04-15 05:12:00 +0530

    fix responsive

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-sheet color="#ffffff" class="pt-8 text-center">
       <v-container>
         <v-row align="center" no-gutters class="mt-8">
-          <v-col cols="6">
+          <v-col cols="12" sm="6" order-sm="first" class="hidden-xs-only">
             <v-layout justify-center align-center>
               <v-flex shrink>
                 <v-img
@@ -18,18 +18,19 @@
             </v-layout>
             <!-- <lottie-animation path="Assets/Home/whatIsMadFest.json" :width="450"/> -->
           </v-col>
-          <v-col class="px-10 align-right HeaderText" cols="6">
-
+          <v-col class="px-8 HeaderText" cols="12" sm="6" order-sm="last">
             <div class="pa-0 MobileText">Mobile</div>
             <div class="pa-0 ApplicationText">Application</div>
             <div class="pa-0 DevelopmentText">Development</div>
-            <div class="pa-0 pb-8 CompetitionText">Competition <i class="mx-1"> &amp </i> Exhibition</div>
+            <div class="pa-0 pb-8 CompetitionText">
+              Competition <i class="mx-1"> &amp </i> Exhibition
+            </div>
 
             <client-only>
               <flip-countdown deadline="2022-06-25 00:00:00"></flip-countdown>
             </client-only>
 
-            <button class="button-2 mt-4 mr-4" role="button">
+            <button class="button-2" role="button">
               <span>Register Now</span>
             </button>
           </v-col>
@@ -46,7 +47,7 @@
     <v-sheet color="#19589D" class="pt-8 pb-8 text-center">
       <v-container>
         <v-row align="center" no-gutters class="mt-8">
-          <v-col class="px-10" cols="6">
+          <v-col class="px-10" cols="12" sm="6">
             <v-card-title class="pa-0 pb-2 cardTitle"
               >What is MAD-FEST?</v-card-title
             >
@@ -64,7 +65,7 @@
               effort.
             </v-card-text>
           </v-col>
-          <v-col cols="6">
+          <v-col cols="12" sm="6">
             <v-layout justify-center align-center>
               <v-flex shrink>
                 <v-img
@@ -81,11 +82,21 @@
           </v-col>
         </v-row>
         <v-row class="dateTime mt-7">
-          <v-col cols="4"
-            ><v-icon>mdi-calender-range</v-icon>25 of May 2022</v-col
+          <v-col cols="12" sm="4">
+            <v-icon color="white" size="35" class="mr-2 mb-2"
+              >mdi-calendar-month-outline</v-icon
+            >25 of May 2022
+          </v-col>
+          <v-col cols="12" sm="4"
+            ><v-icon color="white" size="35" class="mr-2 mb-1"
+              >mdi-clock-time-nine-outline </v-icon
+            >09.00 AM Onwards</v-col
           >
-          <v-col cols="4">09.00 AM Onwards</v-col>
-          <v-col cols="4">Faculty of Computing</v-col>
+          <v-col cols="12" sm="4"
+            ><v-icon color="white" size="35" class="mr-2"
+              >mdi-map-marker-outline </v-icon
+            >Faculty of Computing</v-col
+          >
         </v-row>
       </v-container>
     </v-sheet>
@@ -108,6 +119,7 @@ export default {
   font-weight: 600;
   letter-spacing: -0px;
 }
+
 .cardSubtitle {
   letter-spacing: 0px;
   line-height: 25px;
@@ -150,6 +162,8 @@ export default {
 }
 .button-2 {
   padding: 1rem 3rem;
+  margin-top: 20px;
+  margin-right: 8px;
   text-align: center;
   font-size: 16px;
   text-transform: uppercase;
@@ -184,5 +198,32 @@ export default {
   font-size: 18px !important;
   text-transform: uppercase !important;
   margin-top: 4px !important;
+}
+
+@media only screen and (max-width: 600px) {
+  .cardTitle {
+    color: white;
+    font-size: 30px;
+  }
+  .MobileText {
+    font-weight: 700;
+    font-size: 50px;
+  }
+  .ApplicationText {
+    font-weight: 700;
+    font-size: 50px;
+  }
+  .DevelopmentText {
+    font-weight: 700;
+    font-size: 50px;
+  }
+
+  .CompetitionText {
+    font-weight: 700;
+    font-size: 22px;
+  }
+  .button-2 {
+    margin-top: 30px;
+  }
 }
 </style>
