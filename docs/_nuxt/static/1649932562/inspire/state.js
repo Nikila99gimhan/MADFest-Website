094c5f869cda6f9d04219ac828661a78bdcb8922
SanjulaD <desanjula99@gmail.com>
2022-04-15 00:15:51 +0530

    fix: generate

--- docs/_nuxt/static/1649932562/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMADFest-Website\u002F_nuxt\u002Fstatic\u002F1649932562",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002FMADFest-Website\u002F",assetsPath:"\u002FMADFest-Website\u002F_nuxt\u002F",cdnURL:a}}}}(null));
